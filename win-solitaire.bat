63e8863835c7bf02af83b69161fec42f4160f637
ahgardner <arthurhgardner@gmail.com>
2017-05-07 21:16:05 -0400

    load new version of lib

--- win-solitaire.bat
+++ win-solitaire.bat
@@ -1,4 +1,4 @@
 @REM Command-line solitaire - Clojure - Arthur Gardner 2016.
 @REM Try PowerShell if ANSI codes for red/black symbols do not work.
 chcp 65001
-java -Dfile.encoding=utf-8 -cp target\solitaire-1.0-SNAPSHOT-standalone.jar solitaire.core
+java -Dfile.encoding=utf-8 -cp target\solitaire-1.0-standalone.jar solitaire.core
